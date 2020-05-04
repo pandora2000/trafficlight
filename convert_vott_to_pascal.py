8cc4b6b4e6eeeead3fbb36e56289c10f1e2acbc5
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-04 19:34:51 +0900

    temporary commit [ci skip]

--- convert_vott_to_pascal.py
+++ convert_vott_to_pascal.py
@@ -1,12 +1,4 @@
 import os, shutil, copy, json, cv2, uuid
-vott_path = '/home/zb/works/test/trafficlight/vott-target'
-vott_file_path = f'{vott_path}/trafficlight.vott'
-vott_data = None
-pascal_path = '/home/zb/works/test/trafficlight/pascal/VOCdevkit/VOC2012'
-pascal_anno_path = f'{pascal_path}/Annotations'
-pascal_main_path = f'{pascal_path}/ImageSets/Main'
-pascal_jpg_path = f'{pascal_path}/JPEGImages'
-shutil.rmtree(pascal_path, ignore_errors=True)
 def get_anno_obj_xml_data(name, bndbox):
     return f"""<object>
 		<name>{name}</name>
@@ -38,13 +30,22 @@ def get_anno_xml_data(aid, width, height, objs):
         {obj_xmls}
 	<segmented>0</segmented>
     </annotation>"""
-
-for p in [pascal_anno_path, pascal_main_path, pascal_jpg_path]:
+vott_path = '/home/zb/works/test/trafficlight/vott-target'
+vott_file_path = f'{vott_path}/trafficlight.vott'
+vott_data = None
+pascal_path = '/home/zb/works/test/trafficlight/pascal/myVOCdevkit/VOC2012'
+pascal_anno_path = f'{pascal_path}/Annotations'
+pascal_imgset_path = f'{pascal_path}/ImageSets/Main'
+pascal_jpg_path = f'{pascal_path}/JPEGImages'
+shutil.rmtree(pascal_path, ignore_errors=True)
+for p in [pascal_anno_path, pascal_imgset_path, pascal_jpg_path]:
     os.makedirs(p)
 with open(vott_file_path) as f:
     vott_data = json.loads(f.read())
 classes = ['aeroplane', 'bicycle']
+imgset = []
 for aid in vott_data['assets']:
+    imgset.append(aid)
     a = vott_data['assets'][aid]
     path = a['path'].split(':')[1]
     width = a['size']['width']
@@ -66,3 +67,10 @@ for aid in vott_data['assets']:
     with open(f'{pascal_anno_path}/{aid}.xml', 'w') as f:
         f.write(xml)
     shutil.copyfile(path, f'{pascal_jpg_path}/{aid}.jpg')
+# TODO: きちんと関連するものは別に分ける
+train_cnt = int(round(len(imgset) * 0.8))
+train_imgset = imgset[:train_cnt]
+val_imgset = imgset[train_cnt:]
+for p, imgset in [['train', train_imgset], ['val', val_imgset]]:
+    with open(f'{pascal_imgset_path}/aeroplane_{p}.txt', 'w') as f:
+        f.write('\n'.join([f'{x} -1' for x in imgset]))
