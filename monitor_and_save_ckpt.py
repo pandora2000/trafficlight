aa848eeb8c2bc7ff9fa1cb66aa58ce1f147fe40e
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-06 05:15:27 +0900

    temporary commit [ci skip]

---
+++ monitor_and_save_ckpt.py
@@ -0,0 +1,37 @@
+import argparse, time
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+def get_drive():
+    # to reset authentication, comment in below
+    #!rm -f /content/adc.json
+    auth.authenticate_user()
+    gauth = GoogleAuth()
+    gauth.credentials = GoogleCredentials.get_application_default()
+    return GoogleDrive(gauth)
+def upload_file_to_drive(drive, path):
+    f = drive.CreateFile()
+    f.SetContentFile(path)
+    f.Upload()
+parser = argparse.ArgumentParser()
+parser.add_argument('--root', required=True)
+args = parser.parse_args()
+os.chdir(args.root)
+drive = get_drive()
+latest_index = -1
+while True:
+    for p in os.listdir('.'):
+        if not p.startswith('model.ckpt-') or not p.endswith('.meta'):
+            continue
+        index = int(p.split('-')[1].split('.')[0])
+        if index > latest_index:
+            latest_index = index
+            ps = []
+            for p2 in os.listdir('.'):
+                if not p2.startswith(f'model.ckpt-{index}.'):
+                    continue
+                ps.append(p2)
+            for p2 in ps:
+                upload_file_to_drive(drive, p2)
+    time.sleep(30)
