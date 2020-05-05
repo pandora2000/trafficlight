c977f669495f2ab54986197d33abf19a6778b9b2
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-05 22:51:31 +0900

    temporary commit [ci skip]

--- make_vott_tar.py
+++ make_vott_tar.py
@@ -1,3 +1,3 @@
 import os
 vott_path = '/home/zb/works/test/trafficlight'
-os.system(f'cd {vott_path}; tar -cvf vott.tar target vott-target')
+os.system(f'rm -rf {vott_path}/vott.tar; cd {vott_path}; tar -cvf vott.tar target vott-target; mv {vott_path}/vott.tar ~/downloads/')
