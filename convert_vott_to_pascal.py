f1ce19fd339db667766099d5a53f01a2932b81a8
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-05 22:54:08 +0900

    temporary commit [ci skip]

--- convert_vott_to_pascal.py
+++ convert_vott_to_pascal.py
@@ -78,5 +78,3 @@ for p in ['train', 'val']:
     timgset = [x for x in imgset if x.endswith(f'_{p}.jpg')]
     with open(f'{pascal_imgset_path}/aeroplane_{p}.txt', 'w') as f:
         f.write('\n'.join([f'{x} -1' for x in timgset]))
-os.system(f'cd {pascal_root_path}; tar -cvf myVOCdevkit.tar myVOCdevkit')
-shutil.copyfile(f'{pascal_root_path}/myVOCdevkit.tar', f'/home/zb/downloads/myVOCdevkit.tar')
