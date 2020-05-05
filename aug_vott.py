a55872b4630d0c4f8696d061500d7d4c8e8b79f7
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-05 23:38:40 +0900

    temporary commit [ci skip]

--- aug_vott.py
+++ aug_vott.py
@@ -1,7 +1,4 @@
 import os, shutil, copy, json, cv2, uuid, autoaugment, copy, tensorflow.compat.v1 as tf, numpy as np, argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--root', required=True)
-args = parser.parse_args()
 def get_boxes(width, height, regions):
     boxes = []
     for reg in regions:
@@ -28,87 +25,93 @@ new_data_base = {
         "id": "_YEEMU0Gv"
     }
 }
-vott_src_path = f'{args.root}/vott-target'
-vott_src_file_path = f'{vott_src_path}/trafficlight.vott'
-vott_dst_path = f'{args.root}/vott-aug-target'
-vott_dst_img_path = f'{args.root}/vott-aug-source'
-vott_dst_file_path = f'{vott_dst_path}/trafficlight-aug.vott'
-vott_data = None
-for p in [vott_dst_path, vott_dst_img_path]:
-    shutil.rmtree(p, ignore_errors=True)
-for p in [vott_dst_path, vott_dst_img_path]:
-    os.makedirs(p)
-with open(vott_src_file_path) as f:
-    vott_data = json.loads(f.read())
-cpi = 10
-assets = vott_data['assets']
-new_data = copy.deepcopy(vott_data)
-new_assets = {}
-new_data['assets'] = new_assets
-new_data.update(new_data_base)
-ind = 0
-aids = list(assets.keys())[:]
-# TODO: きちんと関連するものは別に分ける
-train_ind = int(round(len(aids) * 0.8))
-for aid in aids:
-    print(ind)
-    a = assets[aid]
-    path = f'{args.root}/target/{a["name"]}'
-    print(path)
-    nim = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-    height, width = nim.shape[:2]
-    im = tf.convert_to_tensor(nim)
-    anno = None
-    with open(f'{vott_src_path}/{aid}-asset.json') as f:
-        anno = json.loads(f.read())
-    if len(anno['regions']) == 0:
-        boxes = tf.zeros([0, 4])
-    else:
-        boxes = tf.convert_to_tensor(np.array(get_boxes(width, height, anno['regions']), dtype=np.float32))
-    train = ind < train_ind
-    for i in range(cpi if train else 1):
-        if train:
-            # crop入れたいね
-            aug_im, aug_boxes = autoaugment.distort_image_with_autoaugment(im, boxes, 'custom')
-            nnim = cv2.cvtColor(aug_im.numpy(), cv2.COLOR_RGB2BGR)
-            nboxes = aug_boxes.numpy().tolist()
+def main(root_path):
+    vott_src_path = f'{root_path}/vott-target'
+    vott_src_file_path = f'{vott_src_path}/trafficlight.vott'
+    vott_dst_path = f'{root_path}/vott-aug-target'
+    vott_dst_img_path = f'{root_path}/vott-aug-source'
+    vott_dst_file_path = f'{vott_dst_path}/trafficlight-aug.vott'
+    vott_data = None
+    for p in [vott_dst_path, vott_dst_img_path]:
+        shutil.rmtree(p, ignore_errors=True)
+    for p in [vott_dst_path, vott_dst_img_path]:
+        os.makedirs(p)
+    with open(vott_src_file_path) as f:
+        vott_data = json.loads(f.read())
+    cpi = 10
+    assets = vott_data['assets']
+    new_data = copy.deepcopy(vott_data)
+    new_assets = {}
+    new_data['assets'] = new_assets
+    new_data.update(new_data_base)
+    ind = 0
+    aids = list(assets.keys())[:]
+    # TODO: きちんと関連するものは別に分ける
+    train_ind = int(round(len(aids) * 0.8))
+    for aid in aids:
+        print(ind)
+        a = assets[aid]
+        path = f'{root_path}/target/{a["name"]}'
+        print(path)
+        nim = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+        height, width = nim.shape[:2]
+        im = tf.convert_to_tensor(nim)
+        anno = None
+        with open(f'{vott_src_path}/{aid}-asset.json') as f:
+            anno = json.loads(f.read())
+        if len(anno['regions']) == 0:
+            boxes = tf.zeros([0, 4])
         else:
-            nnim = nim
-            nboxes = boxes.numpy().tolist()
-        naid = f'{str(uuid.uuid1()).replace("-", "")}-{"train" if train else "val"}'
-        na = copy.deepcopy(a)
-        na['id'] = naid
-        na['name'] = f'{naid}.jpg'
-        npath = f'{vott_dst_img_path}/{na["name"]}'
-        na['path'] = f'file:{npath}'
-        nh, nw = nnim.shape[:2]
-        na['size'] = {
-            'width': nw,
-            'height': nh,
-        }
-        new_assets[naid] = na
-        cv2.imwrite(npath, nnim)
-        nanno = copy.deepcopy(anno)
-        nanno['asset'] = na
-        for reg, nbox in zip(nanno['regions'], nboxes):
-            t, l, b, r = nbox
-            l, t, r, b = [l * nw, t * nh, r * nw, b * nh]
-            w, h = [r - l, b - t]
-            reg['boundingBox'] = {
-                'left': l,
-                'top': t,
-                'width': w,
-                'height': h
+            boxes = tf.convert_to_tensor(np.array(get_boxes(width, height, anno['regions']), dtype=np.float32))
+        train = ind < train_ind
+        for i in range(cpi if train else 1):
+            if train:
+                # crop入れたいね
+                aug_im, aug_boxes = autoaugment.distort_image_with_autoaugment(im, boxes, 'custom')
+                nnim = cv2.cvtColor(aug_im.numpy(), cv2.COLOR_RGB2BGR)
+                nboxes = aug_boxes.numpy().tolist()
+            else:
+                nnim = nim
+                nboxes = boxes.numpy().tolist()
+            naid = f'{str(uuid.uuid1()).replace("-", "")}-{"train" if train else "val"}'
+            na = copy.deepcopy(a)
+            na['id'] = naid
+            na['name'] = f'{naid}.jpg'
+            npath = f'{vott_dst_img_path}/{na["name"]}'
+            na['path'] = f'file:{npath}'
+            nh, nw = nnim.shape[:2]
+            na['size'] = {
+                'width': nw,
+                'height': nh,
             }
-            reg['points'] = [
-                { 'x': l, 'y': t },
-                { 'x': r, 'y': t },
-                { 'x': r, 'y': b },
-                { 'x': l, 'y': b }
-            ]
-        with open(f'{vott_dst_path}/{naid}-asset.json', 'w') as f:
-            f.write(json.dumps(nanno))
-    ind += 1
-new_data['lastVisitedAssetId'] = list(new_assets.keys())[0]
-with open(vott_dst_file_path, 'w') as f:
-    f.write(json.dumps(new_data))
+            new_assets[naid] = na
+            cv2.imwrite(npath, nnim)
+            nanno = copy.deepcopy(anno)
+            nanno['asset'] = na
+            for reg, nbox in zip(nanno['regions'], nboxes):
+                t, l, b, r = nbox
+                l, t, r, b = [l * nw, t * nh, r * nw, b * nh]
+                w, h = [r - l, b - t]
+                reg['boundingBox'] = {
+                    'left': l,
+                    'top': t,
+                    'width': w,
+                    'height': h
+                }
+                reg['points'] = [
+                    { 'x': l, 'y': t },
+                    { 'x': r, 'y': t },
+                    { 'x': r, 'y': b },
+                    { 'x': l, 'y': b }
+                ]
+            with open(f'{vott_dst_path}/{naid}-asset.json', 'w') as f:
+                f.write(json.dumps(nanno))
+        ind += 1
+    new_data['lastVisitedAssetId'] = list(new_assets.keys())[0]
+    with open(vott_dst_file_path, 'w') as f:
+        f.write(json.dumps(new_data))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', required=True)
+    args = parser.parse_args()
+    main(args.root)
