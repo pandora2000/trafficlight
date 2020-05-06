4d2291726e1a41f3b370a94d492d82e25ced33e3
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-05-06 20:20:20 +0900

    temporary commit [ci skip]

--- monitor_and_save_ckpt.py
+++ monitor_and_save_ckpt.py
@@ -1,22 +1,46 @@
 import argparse, time, os, sys
 print(sys.version)
+from datetime import datetime
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
 def get_drive():
-    # to reset authentication, comment in below
+    # to reset authentication on colab, comment in below
     #!rm -f /content/adc.json
-    auth.authenticate_user()
-    gauth = GoogleAuth()
-    gauth.credentials = GoogleCredentials.get_application_default()
+    gauth = None
+    try:
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+    except:
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()
     return GoogleDrive(gauth)
-def upload_file_to_drive(drive, path):
-    f = drive.CreateFile()
+def upload_file_to_drive(drive, path, params=None):
+    f = None
+    if params is None:
+        f = drive.CreateFile()
+    else:
+        f = drive.CreateFile(params)
     f.SetContentFile(path)
     f.Upload()
+def create_folder_under_id_on_drive(drive, id, name):
+    folder_metadata = {'parents': [{'id': id}], 'title' : name, 'mimeType' : 'application/vnd.google-apps.folder'}
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+    return folder['id']
+def list_drive_folder(drive, id):
+    file_list = drive.ListFile({'q': f"'{id}' in parents and trashed=false"}).GetList()
+    ret = []
+    for f in file_list:
+        ret.append({ 'title': f['title'], 'id': f['id'] })
+    return ret
+def delete_file_from_drive(drive, id):
+    drive.CreateFile({'id': id}).Delete()
 parser = argparse.ArgumentParser()
 parser.add_argument('--root', required=True)
+parser.add_argument('--save_root_id', required=True)
 args = parser.parse_args()
 drive = get_drive()
 latest_index = -1
@@ -24,19 +48,20 @@ while True:
     try:
         os.chdir(args.root)
     except:
+        time.sleep(10)
         continue
-    for p in os.listdir('.'):
-        if not p.startswith('model.ckpt-') or not p.endswith('.meta'):
+    name = datetime.now().strftime('%Y%m%d%H%M%S')
+    print(f'start uploading to {name}...')
+    folder_id = create_folder_under_id_on_drive(
+        drive, args.save_root_id, name
+    )
+    for p in os.listdir():
+        if not os.path.isfile(os.path.join(args.root, p)):
             continue
-        index = int(p.split('-')[1].split('.')[0])
-        if index <= latest_index:
-            continue
-        latest_index = index
-        ps = []
-        for p2 in os.listdir('.'):
-            if not p2.startswith('model.ckpt-' + str(index) + '.'):
-                continue
-            ps.append(p2)
-        for p2 in ps:
-            upload_file_to_drive(drive, p2)
-    time.sleep(30)
+        upload_file_to_drive(drive, p, {'parents': [{'id': folder_id}]})
+    print(f'end upload to {name}...')
+    fs = list_drive_folder(drive, args.save_root_id)
+    fs = sorted(fs, key=lambda x: x['title'])[::-1][2:]
+    for f in fs:
+        delete_file_from_drive(drive, f['id'])
+    time.sleep(30 * 60)
